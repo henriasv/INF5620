253dc4252340de23fee20bfe037afc4d6cc9fb33
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2013-10-08 18:58:27 +0200

    working on tests

--- project2/simpleSolver.py
+++ project2/simpleSolver.py
@@ -2,6 +2,7 @@
 
 from numpy import asarray, zeros, ones, random, meshgrid, linspace, exp
 import numpy as np
+import sys
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import nose.tools as nt
@@ -54,13 +55,37 @@ class user_action_test_constant(user_action):
 	def __call__(self, data2D):
 		nt.assert_almost_equal(np.max(np.max(data2D)), self.constant, self.tolerance)
 
+class user_action_test_symmetric(user_action):
+	def __init__(self, tolerance=1e-12, axis='x'):
+		self.axis = axis
+		self.tolerance = tolerance
+
+	def __call__(self, data2D):
+		Nx, Ny = np.shape(data2D)
+		if self.axis == 'x':
+			tmp_left = data2D[0:Nx/2]
+			tmp_right = np.fliplr(data2D[:,:])[0:Nx/2]
+			tmp_diff = tmp_left-tmp_right;
+			nt.assert_almost_equal(np.max(np.max(tmp_diff)), self.tolerance)
+		elif self.axis == 'y':
+			pass
+		else:
+			nt.assert_almost_equal(0, 1, 1e-12)
+
+class user_action_test_undamped_stsanding_waves(user_action):
+	def __init__(self, tolerance=1e-12):
+		self.tolerance = tolerance
+
+	def __call__(self, data2D):
+		
+
 def update_ghost_points(u):
 	"""
 	This function is designed to apply the neumann boundary condition if du/dn = 0 and the spatial integration scheme is centered
 	"""
-	u[1:-1,0] 		= u[1:-1,2]
+	u[1:-1,0] 	= u[1:-1,2]
 	u[1:-1,-1]	= u[1:-1,-3]
-	u[0,1:-1]		= u[2,1:-1]
+	u[0,1:-1]	= u[2,1:-1]
 	u[-1,1:-1]	= u[-3,1:-1]
 
 def advance_first_step(u0, u1, q, V, b, dx, dt):
@@ -79,6 +104,7 @@ def solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action=None):
 	"""
 	Solver will only give quadratic domains. User should in principle not see that the implementation uses ghost cells!
 	"""	
+	t = 0
 	dx = L/(N-1)
 	x = linspace(-dx, L+dx, N+2)
 	X, Y = meshgrid(x, x)
@@ -92,12 +118,15 @@ def solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action=None):
 	update_ghost_points(um1)
 
 	advance_first_step(um1, u0, q, V, b, dx, dt)
-	user_action.initialize(X, Y, u0)
+	t = dt
+	if not user_action == None:
+		user_action.initialize(X[1:-1, 1:-1], Y[1:-1, 1:-1], u0[1:-1, 1:-1])
 	for i in range(int(float(T)/dt)):
-		advance_mistake_2(um1, u0, u1, q, b, dx, dt)
+		advance(um1, u0, u1, q, b, dx, dt)
+		t += dt
 
-		if user_action:
-			user_action(u0)
+		if not user_action == None:
+			user_action(u0[1:-1, 1:-1])
 
 
 def advance(um1, u0, u1, q, b, dx, dt):
@@ -115,22 +144,20 @@ def advance(um1, u0, u1, q, b, dx, dt):
 
 	update_ghost_points(u0);
 
-
-
-
 def test_constant_solution():
-	c = 1.45
+	constant = 1.45
+	c = 0.8
 	N = 100
 	L = 1.4
 	T = 1
 	dx = float(L)/(N-1)
 	dt = dx/2.01
-	q = lambda X, Y, L: zeros(np.shape(X))+0.8
-	f_I = lambda X, Y, L: zeros(np.shape(X))+c
+	q = lambda X, Y, L: zeros(np.shape(X))+c**2
+	f_I = lambda X, Y, L: zeros(np.shape(X))+constant
 	f_V = lambda X, Y, L: zeros(np.shape(X))
 	f = lambda x: zeros(np.shape(X))
 	b = 0
-	tester = user_action_test_constant(constant=c)
+	tester = user_action_test_constant(constant=constant)
 	solver(f_I, f_V, f, q, b, L, N, dt, T, user_action = tester)	
 
 def advance_mistake_1(um1, u0, u1, q, b, dx, dt):
@@ -178,18 +205,102 @@ def advance_mistake_4(um1, u0, u1, q, b, dx, dt):
 def advance_mistake_5(um1, u0, u1, q, b, dx, dt):
 	pass
 
-if __name__=="__main__":
+def plug_wave_solution(direction='x', user_action=wave_plotter()):
+	# Courant number
+	C = 1.0
 	N = 50
-	L = 1.1
+	L = 1.01
 	T = 10
-	dx = float(L)/(N-1)
-	dt = dx/2.5
-	q = ones((N, N))*0.8
-	f_I = lambda X, Y, L: 3*exp(-200*((X-L/2)**2+(Y-L/2)**2))
-	f_V = lambda X, Y, L: zeros(np.shape(X))
-	f = lambda x: zeros(np.shape(X))
-	q = lambda X, Y, L: ones(np.shape(X))
-	b = 1.2
+	c = 1.1
+	q = lambda X, Y, L: zeros(np.shape(X))+c**2
+	dx = L/(N-1)
+	dt = dx*C/c# Formula to get the correct courant number C = c*dt/dx
+	
+	if (direction == 'x'):
+		f_I = lambda X, Y, L: np.logical_and(X>(L/2-2*L/N),X<(L/2+2*L/N))*1.0
+	elif (direction == 'y'):
+		f_I = lambda X, Y, L: np.logical_and(Y>(L/2-2*L/N),Y<(L/2+2*L/N))*1.0
+	else:
+		print "Neither x nor y provided as direction for plug wave"
 
-	myPlotter = wave_plotter()
-	solver(f_I, f_V, f, q, b, L, N, dt, T, user_action = myPlotter)
+	f_V = lambda X, Y, L: zeros(np.shape(X))
+	f = lambda X, Y, L: zeros(np.shape(X))
+	b = 0
+	solver(f_I, f_V, f, q, b, L, N, dt, T, user_action=user_action)
+
+def test_plug_wave_solution():
+	user_action = user_action_test_symmetric(axis='x')
+	plug_wave_solution(user_action=user_action)
+	user_action = user_action_test_symmetric(axis='y')
+	plug_wave_solution(user_action=user_action)
+
+
+def standing_undamped_waves(user_action = wave_plotter()):
+	h = np.array((0.1, 0.2)) # should be such that L/h is an integer
+	C = 0.5
+	c = 1.0
+	#dx = h
+	dt = h*C/c
+	L = 10.0
+	T = 10
+	A = 1.1
+	m_x = int(2)
+	m_y = int(2)
+	k_x = m_x*np.pi/L
+	k_y = m_y*np.pi/L
+	f_I = lambda X, Y, L: A*np.cos(k_x*X)*np.cos(k_y*Y)
+	f_V = lambda X, Y, L: np.zeros(np.shape(X))
+	f = lambda X, Y, L: np.zeros(np.shape(X))
+	q = lambda X, Y, L: np.zeros(np.shape(X))+c**2
+	b = 0.0 # Undamped waves
+
+	if [L/i for i in h] == [int(L/i) for i in h]:
+		N = L/h+1
+		print N
+	else:
+		print "one or more h-values are not compatible with the L-value such that L/h is integer"
+		sys.exit(1)
+
+
+
+	for i in range(len(N)):
+		solver(f_I, f_V, f, q, b, L, N[i], dt[i], T, user_action)
+
+def test_standing_undamped_waves():
+	user_action;
+
+def read_command_line():
+	if (sys.argv[1] == "plug_wave"):
+		try:
+			direction = sys.argv[2]
+		except:
+			direction = 'x'
+			print "Direction for plug wave was not provided, set to x"
+		plug_wave_solution(direction)
+
+	elif (sys.argv[1] == "standing_undamped_waves"):
+		standing_undamped_waves()
+	else:
+		exit(1)
+
+if __name__=="__main__":
+	try:
+		s = sys.argv[1]
+		read_command_line()
+	except Exception:
+		N = 50
+		L = 1.1
+		T = 10
+		dx = float(L)/(N-1)
+		
+		c = 3.0;
+		dt = dx/c/2.5
+		f_q = lambda X, Y, L: ones(np.shape(X))*c**2
+		f_I = lambda X, Y, L: 3*exp(-200*((X-L/2)**2+(Y-L/2)**2))
+		f_V = lambda X, Y, L: zeros(np.shape(X))
+		f = lambda x: zeros(np.shape(X))
+		q = lambda X, Y, L: ones(np.shape(X))
+		b = 2.01
+
+		myPlotter = wave_plotter()
+		solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action = myPlotter)
