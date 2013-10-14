06622446f7e0a21efeb253f6641f4f763570d3d2
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2013-10-14 14:38:41 +0200

    Visualization works with tvtk

--- project2/simpleSolver.py
+++ project2/simpleSolver.py
@@ -6,6 +6,9 @@ import sys
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import nose.tools as nt
+import time
+from tvtk.api import tvtk
+import copy
 
 
 plt.ion()
@@ -14,7 +17,9 @@ plt.ion()
 class user_action:
 	def initialize(self, X, Y, initial_data=None, vmin=None, vmax=None):
 		pass
-	def __call__(self, data2D):
+	def __call__(self, data2D, t):
+		pass
+	def get(self):
 		pass
 
 class wave_plotter(user_action):
@@ -47,6 +52,104 @@ class wave_plotter(user_action):
 		self.ax.collections.remove(self.myPlot)
 		self.myPlot = self.ax.plot_wireframe(self.X, self.Y, data2D)
 		self.ax.set_zlim3d(self.vmin, self.vmax)
+
+class wave_plotter_tvtk(user_action):
+	def initialize(self, X, Y, initial_data=None, vmin=None, vmax=None):
+		
+		"""
+		Create objects to plot on
+		"""
+		if initial_data == None:
+			initial_data = zeros(shape(X));
+			if vmin == None:
+				self.vmin = -1.0
+			if vmax == None:
+				self.vmax = 1.0
+		else:
+			if vmin == None:
+				self.vmin = np.min(np.min(initial_data))
+			if vmax == None:
+				self.vmax = np.max(np.max(initial_data))
+		x_min = np.min(np.min(X))
+		y_min = np.min(np.min(Y))
+		x_max = np.max(np.max(X))
+		y_max = np.max(np.max(Y))
+		x_middle = (x_min+x_max)/2
+		y_middle = (y_min+y_max)/2
+		z_middle = np.mean(np.mean(initial_data))
+		L = x_max-x_min;
+		diffs = np.shape(X)
+		x_diff = diffs[0]
+		y_diff = diffs[1]
+		z_diff = 1.0#self.vmax-self.vmin
+
+		self.sp = tvtk.StructuredPoints(origin = (x_middle, y_middle, z_middle),
+										dimensions = (x_diff, y_diff, 1), 
+										spacing = (2*L/(x_diff-1), 2*L/(y_diff-1), 100.0))
+
+		self.z = np.transpose(initial_data).flatten()
+		
+		self.sp.point_data.scalars = self.z
+		self.geom_filter = tvtk.ImageDataGeometryFilter(input=self.sp)
+		self.warp = tvtk.WarpScalar(input=self.geom_filter.output)
+		self.normals = tvtk.PolyDataNormals(input=self.warp.output)
+
+		# The rest of the VTK pipeline.
+		self.m = tvtk.PolyDataMapper(input=self.normals.output,
+		                        scalar_range=(self.vmin, self.vmax))
+		self.a = tvtk.Actor(mapper=self.m)
+
+		self.ren = tvtk.Renderer(background=(0.0, 0.0, 0.0))
+		self.ren.add_actor(self.a)
+
+		# Get a nice view.
+		self.cam = self.ren.active_camera
+		self.cam.azimuth(-60)
+		self.cam.roll(90)
+
+		# Create a RenderWindow, add the renderer and set its size.
+		self.rw = tvtk.RenderWindow(size=(800, 800))
+		self.rw.add_renderer(self.ren)
+
+		# Create the RenderWindowInteractor
+		self.rwi = tvtk.RenderWindowInteractor(render_window=self.rw)
+
+		self.rwi.initialize()
+		self.ren.reset_camera()
+		self.rwi.render()
+		#self.rwi.start()
+		
+	def add_static_background(self, data2D):
+		self.sp2 = copy.copy(self.sp)
+		self.z2 = np.reshape(np.transpose(data2D), (-1,))
+		
+		self.sp2.point_data.scalars = self.z2
+		self.geom_filter2 = tvtk.ImageDataGeometryFilter(input=self.sp2)
+		self.warp2 = tvtk.WarpScalar(input=self.geom_filter2.output)
+		self.normals2 = tvtk.PolyDataNormals(input=self.warp2.output)
+
+		# The rest of the VTK pipeline.
+		self.m2 = tvtk.PolyDataMapper(input=self.normals2.output,
+		                        scalar_range=(self.vmin, self.vmax))
+		self.a2 = tvtk.Actor(mapper=self.m2)
+		self.ren.add_actor(self.a2)
+
+		self.rwi.initialize()
+		self.ren.reset_camera()
+		#self.rwi.render()
+		
+
+
+	def __call__(self, data2D, t):
+		self.z[:] = np.reshape(np.transpose(data2D), (-1,))
+		self.m.scalar_range = min(self.z), max(self.z)
+		self.sp.modified()
+		self.rwi.render()
+		#time.sleep(0.1)
+
+	def get(self):
+		self.rwi.start()
+
 
 class user_action_test_constant(user_action):
 	def __init__(self, tolerance=1e-12, constant=0):
@@ -138,6 +241,12 @@ def solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action=None):
 	t = dt
 	if not user_action == None:
 		user_action.initialize(X[1:-1, 1:-1], Y[1:-1, 1:-1], u0[1:-1, 1:-1])
+		try: 
+			user_action.add_static_background(-q[1:-1, 1:-1])
+		except AttributeError as e:
+			print(type(e))
+			raise
+
 	for i in range(int(float(T)/dt)):
 		advance(um1, u0, u1, q, b, dx, dt)
 		t += dt
@@ -254,11 +363,11 @@ def test_plug_wave_solution():
 
 def standing_undamped_waves(user_action = wave_plotter(), h = np.asarray([0.1, 0.05, 0.01, 0.005])):
 	C = 0.5
-	c = 1.0
+	c = 1.1
 	#dx = h
 	dt = h*C/c
 	L = 1.0
-	Nt = 100
+	#Nt = 100
 	T = 1.0
 	A = 1.1
 	m_x = int(2)
@@ -289,20 +398,55 @@ def standing_undamped_waves(user_action = wave_plotter(), h = np.asarray([0.1, 0
 	max_errors = []
 	for i in range(len(N)):
 		solver(f_I, f_V, f, q, b, L, N[i], dt[i], T, user_action)
-		max_errors.append(np.max(user_action.get()))
-		user_action.reset()
-	plt.plot(np.log10(h), np.log10(max_errors))
-
-	plt.xlabel("$\log_{10} \ h$")
-	plt.ylabel("$\log_{10} \ \max(e_{i, j}^n)$")
-	p = np.polyfit(np.log10(h), np.log10(max_errors), 1)
-	plt.title("Order of true error: " + "%.2f" % p[0])
-	plt.show()
-	raw_input("press enter")
+		try:
+			max_errors.append(np.max(user_action.get()))
+			user_action.reset()
+		except:
+			pass
+	try:
+		plt.plot(np.log10(h), np.log10(max_errors))
+		plt.xlabel("$\log_{10} \ h$")
+		plt.ylabel("$\log_{10} \ \max(e_{i, j}^n)$")
+		p = np.polyfit(np.log10(h), np.log10(max_errors), 1)
+		plt.title("Order of true error: " + "%.2f" % p[0])
+		plt.show()
+		raw_input("press enter")
+	except:
+		pass
 
 def convergence_standing_undamped_waves():
 	user_action = user_action_convergence_max_error()
 	standing_undamped_waves(user_action = user_action)
+
+def underwater_hill(B0=1.2, Ba=-0.8, Bmx_scl=0.5, Bmy_scl=0.5, Bs=2.3, b=2.0, shape="gaussian"):
+	N = 100
+	L = 10.0
+	T = 4.0
+	g = 9.81
+	dx = float(L)/(N-1)
+	Bmx = L*Bmx_scl
+	Bmy = L*Bmy_scl
+	
+	c = np.sqrt(g*B0);
+	dt = dx/c/2.5
+	f_q = lambda X, Y, L: ones(np.shape(X))*c**2
+	if (shape == "gaussian"):
+		f_q = lambda X, Y, L: (B0+Ba*np.exp(-(((X-Bmx)/Bs)**2+((Y-Bmy)/(b*Bs))**2)))*g
+
+	if (shape == "cosine_hat"):
+		f_q = lambda X, Y, L: (B0+(Ba*np.cos(np.pi*(X-Bmx)/2/Bs)*np.cos(np.pi*(Y-Bmy)/2/Bs))*((X-Bmx)**2+(Y-Bmy)**2<=Bs**2))*g
+
+	if (shape == "box"):
+		f_q = lambda X, Y, L: (B0+Ba*((Bmx-Bs)<=X)*((Bmx+Bs)>=X)*((Bmy-Bs)<=Y)*((Bmy+Bs)>=Y))*g
+
+	f_I = lambda X, Y, L: 3*exp(-4*(X)**2)
+	f_V = lambda X, Y, L: zeros(np.shape(X))
+	f = lambda x: zeros(np.shape(X))
+	b = 1.1
+
+	myPlotter = wave_plotter_tvtk()
+	solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action = myPlotter)
+	myPlotter.get();
 
 def read_command_line():
 	if (sys.argv[1] == "plug_wave"):
@@ -314,10 +458,18 @@ def read_command_line():
 		plug_wave_solution(direction)
 
 	elif (sys.argv[1] == "standing_undamped_waves"):
-		standing_undamped_waves()
+		standing_undamped_waves(h = np.asarray([0.02]))
 
 	elif (sys.argv[1] == "convergence_standing_undamped_waves"):
 		convergence_standing_undamped_waves()
+
+	elif (sys.argv[1] == "underwater_hill"):
+		try:
+			shape = sys.argv[2]
+			underwater_hill(shape=shape)
+		except IndexError as e:
+			print type(e)
+			underwater_hill(shape = "gaussian")
 	else:
 		exit(1)
 
@@ -325,22 +477,23 @@ if __name__=="__main__":
 	try:
 		s = sys.argv[1]
 		read_command_line()
-	except Exception as e:
+	except IndexError as e:
 		print type(e)
-		raise
-		N = 50
+		#raise
+		N = 100
 		L = 1.1
-		T = 10
+		T = 10.0
 		dx = float(L)/(N-1)
 		
 		c = 3.0;
 		dt = dx/c/2.5
 		f_q = lambda X, Y, L: ones(np.shape(X))*c**2
+		f_q = lambda X, Y, L: 0.1*(X/L)**2*c**2+0.1
 		f_I = lambda X, Y, L: 3*exp(-200*((X-L/2)**2+(Y-L/2)**2))
 		f_V = lambda X, Y, L: zeros(np.shape(X))
 		f = lambda x: zeros(np.shape(X))
-		q = lambda X, Y, L: ones(np.shape(X))
 		b = 2.01
 
-		myPlotter = wave_plotter()
+		myPlotter = wave_plotter_tvtk()
 		solver(f_I, f_V, f, f_q, b, L, N, dt, T, user_action = myPlotter)
+		myPlotter.get();
