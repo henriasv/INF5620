4ac3b34dc1ccfb251d21984620f7471cc039de6e
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2013-12-05 17:41:47 +0100

    Added diffusion of gaussian

--- final_project/src/diffusion_test_manufactured_solution.py
+++ final_project/src/diffusion_test_manufactured_solution.py
@@ -9,8 +9,18 @@ import matplotlib.pyplot as plt
 
 from dolfin import *
 
-def alpha(u):
-	return 1 + u*u
+class alpha:
+	def __init__(self):
+		pass
+	def __call__(self, u):
+		return 1 + u*u
+
+class alpha2(alpha):
+	def __init__(self, beta):
+		self.beta = beta
+
+	def __call__(self, u):
+		return 1 + self.beta*u*u
 
 class User_Action:
 	def __init__(self, function=None):
@@ -18,10 +28,13 @@ class User_Action:
 		self.h = None
 		self.t = None
 
-	def act(self, u):
+	def act_time_loop(self,t, u,V):
 		pass
 
-class User_Action_different_t(User_Action):
+	def act_after_time_loop(self, t, u, V, h):
+		pass
+
+class User_Action_convergence_dt(User_Action):
 	def __init__(self, u_e):
 		self.u_e = u_e
 
@@ -35,20 +48,36 @@ class User_Action_different_t(User_Action):
 	def act_time_loop(self, t, u, V):
 		return False
 
-class User_Action_convercence_dt(User_Action):
+class User_Action_different_t(User_Action):
 	def __init__(self, u_e):
 		self.u_e = u_e
 
+	def act_after_time_loop(self, t, u, V, h):
+		self.h = h
+		u_e.t = u_e_in_V = interpolate(u_e, V)
+
+class User_Action_plot(User_Action):
+	def __init__(self, minmax):
+		self.minmax = minmax
+
+	def act_time_loop(self, t, u, V):
+		wiz_w = plot(u)
+		wiz_w.set_min_max(*self.minmax)
+		return False
 
 
-def solve_nonlinear_diffusion(degree, divisions, alpha, source_function, rho, isPlot=False, T=1.0, user_action=User_Action()):
+def solve_nonlinear_diffusion(degree, divisions, alpha, 
+								source_function=Constant(0.0), 
+								rho=1.0, 
+								isPlot=False, 
+								T=1.0, 
+								user_action=User_Action(), 
+								u0 = Constant(0.0)):
 	d = len(divisions)
 	domain_type = [UnitIntervalMesh, UnitSquareMesh, UnitCubeMesh]
 	mesh = domain_type[d-1](*divisions)
 	V = FunctionSpace(mesh, 'Lagrange', degree)
-	u0 = Constant(0.0)
 	u1 = interpolate(u0, V)
-
 	t = 0
 	h = (1.0/(divisions[0]-1))**2
 	print h
@@ -71,7 +100,7 @@ def solve_nonlinear_diffusion(degree, divisions, alpha, source_function, rho, is
 	# Effectively N iteration picard method inside time loop
 	while t<T:
 		f.t = t;
-		for i in range(3):
+		for i in range(1):
 			solve(a==L, u)
 			u_k.assign(u)
 		u1.assign(u)
@@ -96,10 +125,10 @@ def read_command_line():
 		# run default test
 			divisions = [[5*i] for i in range(1, 10, 1)]
 			hs = []; errors = []
-			my_user_action = User_Action_different_t(u_e)
+			my_user_action = User_Action_convergence_dt(u_e)
 
 			for division in divisions:
-				user_action = solve_nonlinear_diffusion(1, division, alpha, f, rho, isPlot=False, user_action=my_user_action, T = 0.05)
+				user_action = solve_nonlinear_diffusion(1, division, alpha, source_function=f, rho=rho, user_action=my_user_action, T = 0.05)
 				hs.append(user_action.h); errors.append(user_action.E)
 			print hs, errors
 			hs = numpy.log10(numpy.asarray(hs)); errors = numpy.log10(numpy.asarray(errors))
@@ -112,10 +141,15 @@ def read_command_line():
 			plt.ylabel(r"$\log_{10}(E)$")
 			plt.show()
 
-	if len(sys.argv) == 2:
-		
+		if (sys.argv[1] == "different_t"):
 			divisions = [20, 20]
-			t = [0.1, 1, 4]
+			t = [0.1, 1, 4, 7]
+
+		if (sys.argv[1] == "gaussian"):
+			division = [50, 50]
+			u0 = Expression("exp(-0.5/pow(sigma, 2)*(x[0]*x[0]+x[1]*x[1]))", sigma=0.1)
+			my_user_action = User_Action_plot((0.0, 0.1))
+			user_action = solve_nonlinear_diffusion(1, division, alpha2(100000.0), user_action=my_user_action, T=0.05, u0 = u0)
 
 if __name__=="__main__":
 	read_command_line()
